1c5b5c879670201226bd1efe70fe4a4f27b41258
caticoa3 <caticoa3@gmail.com>
2018-09-29 14:52:53 -0700

    Correct error in umap_plot.py that was breaking flask_app.py

--- src/umap_plots.py
+++ src/umap_plots.py
@@ -103,8 +103,8 @@ def umap_bokeh(bn_feat, #= pd.read_csv('../results/prod_test_feat.csv', index_co
         size=4
     )
     #    show(plot_figure)
-#    script, div = components(plot_figure)
-    html = file_html(plot_figure, INLINE)
+    script, div = components(plot_figure)
+#    html = file_html(plot_figure, INLINE)
 
     return html #script, div
 
